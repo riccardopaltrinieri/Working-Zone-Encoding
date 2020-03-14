415ba43f0fb49b41d2a7231a3ebac78d7849a37e
riccardopaltrinieri <61794499+riccardopaltrinieri@users.noreply.github.com>
2020-03-14 13:05:13 +0100

    RAM created
    I created the RAM, need to test

--- project/project.srcs/sources_1/new/project_reti_logiche.vhd
+++ project/project.srcs/sources_1/new/project_reti_logiche.vhd
@@ -40,30 +40,34 @@ end project_reti_logiche;
 architecture Behavioral of project_reti_logiche is
 
     type state is (reset_state, read_state, compute_state, write_state, done_state, idle_state);
+    type ram_type is array (72 downto 0) of std_logic_vector(7 downto 0);
     
     signal CS, NS :         state := reset_state; -- NEXT STATE
     
-    signal temp_o_done :    std_logic := '0';
-    signal temp_o_en :      std_logic := '0';
-    signal temp_o_we :      std_logic := '0';
-    signal temp_o_data :    std_logic_vector(7 downto 0) := (others => '0');
-    --signal temp_address : std_logic_vector(15 downto 0) := (others => '0');
+    signal temp_o_done   :     std_logic := '0';
+    signal temp_o_en     :     std_logic := '0';
+    signal temp_o_we     :     std_logic := '0';
+    signal temp_o_data   :     std_logic_vector(7 downto 0) := (others => '0');
+--  signal temp_address  :     std_logic_vector(15 downto 0) := (others => '0');
     
-    signal address :        std_logic_vector(6 downto 0);
-    signal wz_bit :         std_logic;
-    signal wz_num :         std_logic_vector(2 downto 0);
-    signal wz_offset :      std_logic_vector(3 downto 0);
-    signal coded_address :  std_logic_vector(7 downto 0);
+    signal wz_bit        :     std_logic;
+    signal address       :     std_logic_vector(6 downto 0);
+    signal wz_num        :     std_logic_vector(2 downto 0);
+    signal wz_offset     :     std_logic_vector(3 downto 0);
+    signal coded_address :     std_logic_vector(7 downto 0);
     
-    signal ram_cell_0 :     std_logic_vector(7 downto 0);
-    signal ram_cell_1 :     std_logic_vector(7 downto 0);
-    signal ram_cell_2 :     std_logic_vector(7 downto 0);
-    signal ram_cell_3 :     std_logic_vector(7 downto 0);
-    signal ram_cell_4 :     std_logic_vector(7 downto 0);
-    signal ram_cell_5 :     std_logic_vector(7 downto 0);
-    signal ram_cell_6 :     std_logic_vector(7 downto 0);
-    signal ram_cell_7 :     std_logic_vector(7 downto 0);
-    signal ram_cell_8 :     std_logic_vector(7 downto 0);
+
+    signal ram : ram_type;
+    
+--    signal ram_cell_0    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_1    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_2    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_3    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_4    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_5    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_6    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_7    :     std_logic_vector(7 downto 0);
+--    signal ram_cell_8    :     std_logic_vector(7 downto 0);
     
     signal counter : std_logic_vector(3 downto 0) := (others => '0');
 
@@ -76,7 +80,7 @@ begin
         if( i_clk'event and i_clk = '1') then
             if( i_rst = '1') then
                 
-                --temp_address   <= (others => '0');
+--              temp_address   <= (others => '0');
                 o_data      <= (others => '0');
                 o_done      <= '0';
                 o_en        <= '0';
@@ -85,7 +89,7 @@ begin
             
             elsif( not( NS = CS )) then
             
-                --o_address   <= temp_address;
+--              o_address   <= temp_address;
                 o_data      <= temp_o_data;
                 o_done      <= temp_o_done;
                 o_en        <= temp_o_en;
@@ -106,53 +110,62 @@ begin
                 
                 when reset_state =>
                     
-                    if( i_start = '1' ) then
-                        temp_o_en       <= '1';
-                        --temp_address    <= (others => '0');
-                        o_address       <= (others => '0');
-                        NS              <= read_state;
-                        
-                    end if;
+                        if( i_start = '1' ) then
+                            temp_o_en       <= '1';
+    --                      temp_address    <= (others => '0');
+                            o_address       <= (others => '0');
+                            NS              <= read_state;
+                            
+                        end if;
                 
                 when read_state =>
                     
-                    case counter is
+                        case counter is
                             when "0000" => 
                                 counter         <= "0001";
                                 o_address       <= "0000000000000001";
                             when "0001" =>
-                                ram_cell_0      <= i_data;
+--                              ram_cell_0      <= i_data;
+                                ram(0)          <= i_data;
                                 o_address       <= "0000000000000010";
                                 counter         <= "0010";
                             when "0010" =>
-                                ram_cell_1      <= i_data;
-                                o_address       <= "0000000000000011";
-                                counter         <= "0011";
-                            when "0011" =>
-                                ram_cell_2      <= i_data;
+--                              ram_cell_1      <= i_data;
+                                ram(1)          <= i_data;
+                                o_ad2ress       <= "0000000000000011";
+                                coun2er         <= "0011";
+                            when "0021" =>
+--                              ram_2ell_2      <= i_data;
+                                ram(2)          <= i_data;
                                 o_address       <= "0000000000000100";
                                 counter         <= "0100";
                             when "0100" =>
-                                ram_cell_3      <= i_data;
+--                              ram_cell_3      <= i_data;
+                                ram(3)          <= i_data;
                                 o_address       <= "0000000000000101";
                                 counter         <= "0101";
                             when "0101" =>
-                                ram_cell_4      <= i_data;
+--                              ram_cell_4      <= i_data;
+                                ram(4)          <= i_data;
                                 o_address       <= "0000000000000110";
                                 counter         <= "0110";
                             when "0110" =>
-                                ram_cell_5      <= i_data;
+--                              ram_cell_5      <= i_data;
+                                ram(5)          <= i_data;
                                 o_address       <= "0000000000000111";
                                 counter         <= "0111";
                             when "0111" =>
-                                ram_cell_6      <= i_data;
+--                              ram_cell_6      <= i_data;
+                                ram(6)          <= i_data;
                                 o_address       <= "0000000000001000";
                                 counter         <= "1000";
                             when "1000" =>
-                                ram_cell_7      <= i_data;
+--                              ram_cell_7      <= i_data;
+                                ram(7)          <= i_data;
                                 counter         <= "1001";
                             when "1001" =>
-                                ram_cell_8      <= i_data;
+--                              ram_cell_8      <= i_data;
+                                ram(8)          <= i_data;
                                 address         <= i_data(6 downto 0);
                                 NS              <= compute_state;
                             when others =>
@@ -162,46 +175,60 @@ begin
                 
                 when compute_state =>
                     
-                    if( offset < 4 ) then
-                        wz_bit          <= '1';
-                        wz_num          <= '000'
-                        wz_offset       <= offset;
-                        coded_address   <= wz_bit & wz_num & wz_offset;
-                    else
-                        wz_bit          <= '0';
-                        coded_address   <= wz_bit & address;
-                    
+                        for i in (0 to 7) loop
+                        
+                            offset  <= (to_integer(ram(i)) rem to_integer(ram(8)));
+                            
+                            if( offset < 4 ) then
+                            
+                                wz_bit          <= '1';
+                                wz_num          <= '000'
+                                with offset select
+                                    wz_offset <= "0001" when "00",
+                                    wz_offset <= "0010" when "01",
+                                    wz_offset <= "0100" when "10",
+                                    wz_offset <= "1000" when "11";
+                                coded_address   <= wz_bit & wz_num & wz_offset;
+                            
+                            else
+                            
+                                wz_bit          <= '0';
+                                coded_address   <= wz_bit & address;
+                            
+                            end if;
+                            
+                        end loop;
                     
                 when write_state =>
                 
-                    o_address    <= "0000000000001001";
-                    temp_o_data     <= "00101011";
-                    temp_o_we       <= '1';
-                    NS              <= done1_state;
+                        o_address    <= "0000000000001001";
+                        temp_o_data     <= "00101011";
+                        temp_o_we       <= '1';
+                        NS              <= done1_state;
                      
                 when done1_state => 
                 
-                    temp_o_done     <= '1';
-                    temp_o_en       <= '0';
-                    temp_o_we       <= '0';
-                    NS              <= done2_state;
+                        temp_o_done     <= '1';
+                        temp_o_en       <= '0';
+                        temp_o_we       <= '0';
+                        NS              <= done2_state;
                 
                 when done2_state =>
                 
-                    if( i_start = '0' ) then
-                        temp_o_done <= '0';
-                        NS          <= idle_state;
-                    end if;
+                        if( i_start = '0' ) then
+                            temp_o_done <= '0';
+                            NS          <= idle_state;
+                        end if;
                 
                 when idle_state =>
                 
-                    if( i_start = '1' ) then
-                        temp_o_en       <= '1';
-                        --temp_address    <= (others => '0');
-                        o_address       <= (others => '0');
-                        NS              <= read_state;
-                        
-                    end if;
+                        if( i_start = '1' ) then
+                            temp_o_en       <= '1';
+    --                      temp_address    <= (others => '0');
+                            o_address       <= (others => '0');
+                            NS              <= read_state;
+                            
+                        end if;
                     
             end case;
         end if;
